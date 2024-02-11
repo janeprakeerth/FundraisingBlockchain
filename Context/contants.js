36f71f34489a46b4cf1ff22f9fd6ffc4f3579b7c
janep <bp5421@srmist.edu.in>
2024-02-11 18:34:04 +0530

    first commit

---
+++ Context/contants.js
@@ -0,0 +1,6 @@
+import crowdFunding from "./CrowdFunding.json";
+
+// Crowd Funding MarketPlace
+
+export const CrowdFundAddress  = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const CrowdFundingAbi = crowdFunding.abi;
